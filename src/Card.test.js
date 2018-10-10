1ca2d49b3a8564e0d42b988b9cc13be803310162
Evgenii Mamenko <38508413+zhenya-mamenko@users.noreply.github.com>
2018-10-10 21:45:01 +0700

    Update tests

--- src/Card.test.js
+++ src/Card.test.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { shallow, configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import Card from './Card.jsx';
+
+configure({ adapter: new Adapter() });
 
 it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<Card id="13060" image="test.jpg" name="Margarita" category="Cocktail" ingredients="Tequila, Salt" />, div);
     ReactDOM.unmountComponentAtNode(div);
+});
+
+it('should show Extended card', () => {
+    const card = shallow(<Card id="13060" image="test.jpg" name="Margarita" category="Cocktail" ingredients="Tequila, Salt" />);
+    card.find('.Card').simulate('click', { preventDefault: () => {} });
+    card.find('.Card').simulate('keydown', { key: 'Esc', preventDefault: () => {} });
 });
--- src/ExtendedCard.test.js
+++ src/ExtendedCard.test.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { shallow, configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import ExtendedCard from './ExtendedCard.jsx';
+
+configure({ adapter: new Adapter() });
 
 it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<ExtendedCard id="13060" />, div);
     ReactDOM.unmountComponentAtNode(div);
 });
+
+it('loads data from API', () => {
+    const extendedCard = shallow(<ExtendedCard id="13060" />);
+    expect(extendedCard.state.cocktail).not.toBeNull();
+});
+
